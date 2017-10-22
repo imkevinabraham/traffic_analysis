9dfd81ab705a506d855c45e537bb5214625289ea
Kevin Abraham <kevin@kevinabraham.com>
2017-10-22 17:23:19 +1100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # traffic_analysis
 Traffic Analysis on Video using openCV and python3
+
+This repository is for doing traffic analysis on a video feed.
+
+It does Heatmaps and counts number of people who cross a line in a video.
+
+Requirements
+- OpenCV
+- Python 3
+- numpy
+
+
+Installation
+1. git clone https://github.com/imkevinabraham/traffic_analysis.git
+2. If you want to use your own video change `cap = cv2.VideoCapture('peopleWalking.mov')` to whatever your video file name is
+
+
+Traffic counter:
+change directory to the folder then run
+`python trafficCounter.py`
+
+Heatmap:
+change directory to the folder then run
+`python heatmap.py`
---
+++ heatmap.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cv2
+
+class Motion:
+    
+    def __init__(self):                  
+                    print("Motion Detection Object Created")    
+                    #input file name of video
+                    self.inname= 'peopleWalking.mov'
+    
+                    #file name to save
+                    self.outname = "output"
+    
+    def prep(self):
+        
+        #just read the first frame to get height and width
+        cap = cv2.VideoCapture(self.inname)     
+                
+        ret,self.orig_image = cap.read()
+        width = np.size(self.orig_image, 1)
+        height = np.size(self.orig_image, 0)
+        frame_size=(height, width)           
+        
+        #make accumulator image of the same size
+        self.accumulator =  np.zeros((height, width), np.float32) # 32 bit accumulator
+
+                
+    def run(self):
+        cap = cv2.VideoCapture(self.inname)
+        fgbg = cv2.createBackgroundSubtractorMOG2(varThreshold=80,detectShadows=False)
+        while(1):
+            ret, frame = cap.read()
+            if not ret:
+                break
+            fgmask = fgbg.apply(frame)
+            self.accumulator=self.accumulator+fgmask
+    def write(self):
+        
+        self.ab=cv2.convertScaleAbs(255-np.array(self.accumulator,'uint8'))  
+        
+        #only get reasonable high values, above mean
+        ret,self.acc_thresh=cv2.threshold(self.ab,self.ab.mean(),255,cv2.THRESH_TOZERO)
+        
+        #make a color map
+        acc_col = cv2.applyColorMap(self.acc_thresh,cv2.COLORMAP_HOT)
+        
+        cv2.imwrite(str(self.outname + "/heatmap.jpg"),acc_col)
+        
+        #add to original frame
+        backg = cv2.addWeighted(np.array(acc_col,"uint8"),0.45,self.orig_image,0.55,0)
+        
+        cv2.imwrite(str(self.outname + "/heatmap_background.jpg"),backg)
+        
+#==================
+# MAIN ENTRY POINT
+#==================
+
+if __name__ == "__main__":
+    motionVid=Motion()
+    motionVid.prep()    
+    motionVid.run()
+    motionVid.write()
